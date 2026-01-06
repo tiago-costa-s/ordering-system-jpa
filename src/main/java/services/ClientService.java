aa37ca726303ccd15ce62329259b6588272b8e38
Tiago Costa <tiagocostasantos0013@gmail.com>
2026-01-06 20:06:52 -0300

    Funcionalidade: adicionar DAO de cliente e serviço com validação de e-mail.

--- src/main/java/services/ClientService.java
+++ src/main/java/services/ClientService.java
@@ -1,5 +1,24 @@
 package services;
 
+import dao.ClientDAO;
+import entities.Client;
+
 public class ClientService {
 
+	private final ClientDAO clientDAO = new ClientDAO();
+
+	public void createCliente(Client newClient) {
+
+		if (newClient == null) {
+			throw new IllegalArgumentException("Cliente não pode ser nulo.");
+		}
+
+		Client validateEmail = clientDAO.findByEmail(newClient.getEmail());
+
+		if (validateEmail != null) {
+			throw new IllegalArgumentException("Existe cliente cadastrado com o e-mail informado.");
+		}
+
+		clientDAO.insertClient(newClient);
+	}
 }
